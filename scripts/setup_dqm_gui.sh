5d0c60869acf30ae71a922ca080042a73a71135d
kreczko <lkreczko@googlemail.com>
2018-05-21 13:23:33 +0100

    added printout alerting when DQM GUI is started

--- scripts/setup_dqm_gui.sh
+++ scripts/setup_dqm_gui.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 ${CODE_DIR}/Deploy -r "comp=comp.pre" -A ${SCRAM_ARCH} -R comp@${DQM_TAG} -t ${DQM_TAG} -s "prep sw post" ${INSTALL_DIR} dqmgui/bare
 
+echo "Deployed DQM GUI"
 # mv ${INSTALL_DIR}/current/config/dqmgui{,.bak}
 # ln -s ${CODE_DIR}/dqmgui ${INSTALL_DIR}/current/config/dqmgui
