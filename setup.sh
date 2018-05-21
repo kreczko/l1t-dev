407b905c1e1106a0eaaebc90bbff43d576d3383c
kreczko <lkreczko@googlemail.com>
2018-05-21 13:23:53 +0100

    updated setup to CMSSW 10.2.0

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 export CVMFS_TMP_DIR=/tmp/cvmfs-l1t
 export L1T_DEV_DIR=$PWD/dev
 export USERID=`id -u`
-export CMSSW_VERSION="CMSSW_10_0_0"
+export CMSSW_VERSION="CMSSW_10_2_0_pre3"
 export SCRAM_ARCH="slc6_amd64_gcc630"
 
 mkdir -p ${CVMFS_TMP_DIR}
